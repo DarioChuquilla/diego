5663be7b2a82989e27ce11e4d411627380fe55e6
Dario Chuquilla <dchuquilla@nettheory.com>
2014-08-06 12:23:43 -0500

    First commit for PG fine.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diego::Application.config.secret_token = 'e08f4167105c5f94527e925f3e68a07f7e8f039c04eea7cc55559830e5531105d93ad60c4ae0b94d18f8eacfba04b5b1d0a68e262f288a2d029c0c58c4511410'
