dc5f86da77bf92ba08fdcf2535e016280bad86b3
dchuquilla <dchuquilla@nettheory.com>
2014-08-07 02:18:05 -0500

    revert hotels

--- app/models/admin/hotel.rb
+++
@@ -1,3 +0,0 @@
-class Admin::Hotel < ActiveRecord::Base
-  attr_accessible :address, :description, :latitude, :longitude, :name
-end
--- test/unit/helpers/admin/hotels_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Admin::HotelsHelperTest < ActionView::TestCase
-end
