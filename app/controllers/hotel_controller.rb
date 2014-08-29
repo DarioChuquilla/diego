b4a6ed797f363902e5b2dbd963806d882409c668
Dario Chuquilla <dchuquilla@nettheory.com>
2014-08-29 18:00:14 -0500

    Circular responsive map canvas

--- app/controllers/hotel_controller.rb
+++ app/controllers/hotel_controller.rb
@@ -22,7 +22,8 @@ class HotelController < ApplicationController
   end
 
   def get_hotels_nearby
-    @radious = (params[:distance].to_f) / 1000 #Distance is requested in meters, setting it to KMs
+    # @radious = (params[:distance].to_f) / 1000 #Distance is requested in meters, setting it to KMs
+    @radious = ((params[:distance].to_f * 1.02) / 1000.0) * Math.sin(Geocoder::Calculations.to_radians(45)) #Distance for circular maps canvas
     @hotels = Hotel.active.near [params[:lat], params[:lng]], @radious, :units => :km
     respond_to do |format|
       format.js # show.html.erb
