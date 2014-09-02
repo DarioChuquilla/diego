b38277930c8999aa8b402a7735e8349de08af965
Dario Chuquilla <dchuquilla@nettheory.com>
2014-09-02 16:13:16 -0500

    Map in square form

--- app/controllers/hotel_controller.rb
+++ app/controllers/hotel_controller.rb
@@ -29,7 +29,9 @@ class HotelController < ApplicationController
   def get_hotels_nearby
     # @radious = (params[:distance].to_f) / 1000 #Distance is requested in meters, setting it to KMs
     @radious = ((params[:distance].to_f * 1.02) / 1000.0) * Math.sin(Geocoder::Calculations.to_radians(45)) #Distance for circular maps canvas
-    @hotels = Hotel.active.near [params[:lat], params[:lng]], @radious, :units => :km
+    box = Geocoder::Calculations.bounding_box([params[:lat], params[:lng]], @radious, :units => :km)
+    # @hotels = Hotel.active.near [params[:lat], params[:lng]], @radious, :units => :km
+    @hotels = Hotel.active.within_bounding_box(box)
     respond_to do |format|
       format.js # show.html.erb
       format.json { render json: @hotels }
