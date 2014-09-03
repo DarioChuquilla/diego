e38d5cd4d563ce5c84f5dc82a19d08fd77af8519
Dario Chuquilla <dchuquilla@nettheory.com>
2014-09-03 14:37:13 -0500

    Navbar filtering by hotel type

--- app/controllers/hotel_controller.rb
+++ app/controllers/hotel_controller.rb
@@ -4,7 +4,9 @@ class HotelController < ApplicationController
   end
 
   def nearest
-    @hotels = Hotel.where('hotels.active = ?', true)
+    @type_name = params[:type_name].present? ? params[:type_name] : 'Motel'
+    logger.debug{"============ TYPE_NAME #{@type_name.inspect}"}
+    @hotels = Hotel.active.where('hotels.type_name = ?', @type_name)
   end
 
   def room
@@ -30,8 +32,10 @@ class HotelController < ApplicationController
     # @radious = (params[:distance].to_f) / 1000 #Distance is requested in meters, setting it to KMs
     @radious = ((params[:distance].to_f * 1.02) / 1000.0) * Math.sin(Geocoder::Calculations.to_radians(45)) #Distance for circular maps canvas
     box = Geocoder::Calculations.bounding_box([params[:lat], params[:lng]], @radious, :units => :km)
-    # @hotels = Hotel.active.near [params[:lat], params[:lng]], @radious, :units => :km
-    @hotels = Hotel.active.within_bounding_box(box)
+    # @hotels = Hotel.active.near [params[:lat], params[:lng]], @radious, :units => :km #for circular map
+    @type_name = params[:type_name].present? ? params[:type_name] : 'Motel'
+
+    @hotels = Hotel.active.where('hotels.type_name = ?', @type_name).within_bounding_box(box)
     respond_to do |format|
       format.js # show.html.erb
       format.json { render json: @hotels }
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,9 +2,6 @@ class Hotel < ActiveRecord::Base
   attr_accessible :description, :latitude, :longitude, :name, :telephone, :email, :type_name
   validates_presence_of :name, :description, :type_name
 
-
-
-
   belongs_to :user
   has_many :promotions
   has_many :rooms
